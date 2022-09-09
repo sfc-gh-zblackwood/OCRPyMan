5b9c0a84ad82936fe11b399d6b25fa3833d85865
Jean-Paul BELLA <jean.paul.bella@hotmail.fr>
2022-09-09 08:24:33 +0200

    Non working start

--- notebooks/letter_detection_utils.py
+++ notebooks/letter_detection_utils.py
@@ -10,24 +10,25 @@ def load_image(filepath):
 
 
 @tf.function
-def preprocess(filepath, imgSize=(32, 128), dataAugmentation=False, scale=0.8, isthreshold=False):
-
-    img = load_image(filepath)/255
+def preprocess(filepath, img_size=(32, 128), data_augmentation=False, scale=0.8, is_threshold=False):
+    img = load_image(filepath)/255 # To work with values between 0 and 1
+    img_original_size = tf.shape(img)
 
     # there are damaged files in IAM dataset - just use black image instead
     if img is None:
-        img = tf.ones([imgSize[0], imgSize[1], 1])
+        img = tf.ones([img_size[0], img_size[1], 1])
+        # should return ?
 
     # increase dataset size by applying random stretches to the images
 
-    if dataAugmentation:
+    if data_augmentation:
         stretch = scale*(tf.random.uniform([1], 0, 1)[0] - 0.3) # -0.5 .. +0.5
-        wStretched = tf.maximum(int(float(tf.shape(img)[0]) * (1 + stretch)), 1) # random width, but at least 1
-        img = tf.image.resize(img, (wStretched, tf.shape(img)[1])) # stretch horizontally by factor 0.5 .. 1.5
+        w_stretched = tf.maximum(int(float(img_original_size[0]) * (1 + stretch)), 1) # random width, but at least 1
+        img = tf.image.resize(img, (w_stretched, img_original_size[1])) # stretch horizontally by factor 0.5 .. 1.5
 
 
     # create target image and copy sample image into it
-    (wt, ht) = imgSize
+    (wt, ht) = img_size
 
     w, h = float(tf.shape(img)[0]), float(tf.shape(img)[1])
 
@@ -45,17 +46,18 @@ def preprocess(filepath, imgSize=(32, 128), dataAugmentation=False, scale=0.8, i
     dx = wt - newSize[0]
     dy = ht - newSize[1]
 
-    if dataAugmentation :
+    # Add padding
+    if data_augmentation :
         dx1=0
         dy1=0
-        if dx!=0:
+        if dx != 0:
             dx1 = tf.random.uniform([1], 0, dx, tf.int32)[0]
-        if dy!=0:
+        if dy != 0:
             dy1 = tf.random.uniform([1], 0, dy, tf.int32)[0]
         img = tf.pad(img[..., 0], [[dx1, dx-dx1], [dy1, dy-dy1]], constant_values=1)
     else :
         img = tf.pad(img[..., 0], [[0, dx], [0, dy]], constant_values=1)
 
-    if isthreshold:
+    if is_threshold:
         return tf.expand_dims(1-(1-img)*tf.cast(img < 0.8, tf.float32), -1)
     return tf.expand_dims(img, -1)
