76edab5e3827e8cfefe26851873f48323b35d751
Jean-Paul BELLA <jean.paul.bella@hotmail.fr>
2022-09-18 20:59:55 +0200

    Update

--- notebooks/letter_detection_utils.py
+++ notebooks/letter_detection_utils.py
@@ -1,13 +1,130 @@
 import tensorflow as tf
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+import pickle
+import pandas as pd
+import re
+import contextlib
+import os
+import matplotlib.patches as patches
 
+
+def plot_avg_width_per_string_length(df):
+    biggest_word_size = df['length'].max()
+    width_means = []
+    for l in range(1, biggest_word_size):
+        width_means.append(df[df['length'] == l].w.mean())
+        
+    xaxis = range(1, biggest_word_size)
+    plt.figure(figsize=(20,14))
+    plt.title('Variation de la largeur en fonction de la taille du mot représenté')
+    plt.xlabel('Taille du mot')
+    plt.ylabel('Largeur du mot')
+    plt.plot(xaxis, width_means, ls='--', color='navy');
+
+def show_df_form_img(df, row_index):
+    selected_row = df.iloc[row_index]
+    img = plt.imread(selected_row.form_img_path)
+    fig, ax = plt.subplots(figsize=(20,15))
+    fig.figsize=(20,10)
+    ax.imshow(img, cmap='gray')
+    ax.add_patch(
+    patches.Rectangle(
+        (selected_row.x-8, selected_row.y-8),
+        selected_row.w+16,
+        selected_row.h+16,
+        fill=False,
+        color = 'red'      
+    ) ) 
+    plt.axis('off')
+    plt.show()
+
+def show_df_word_img(df, row_index):
+    selected_row = df.iloc[row_index]
+    img = plt.imread(selected_row.word_img_path)
+    plt.figure(figsize = (10,8))
+    plt.title("Texte: \"{}\" au format {} avec h={}, w={}".format(selected_row.transcription, img.shape, selected_row.h, selected_row.w));
+    plt.axis('off')
+    plt.imshow(img, cmap='gray');
+
+def silence_method_call(callback=None, cargs=()):
+    with open(os.devnull, 'w') as devnull_file:
+        with contextlib.redirect_stdout(devnull_file):
+            if callback is not None:
+                return callback(*cargs)
+
+def process_df_img(df, img_size = (32, 128)):
+    nb_features = img_size[0] * img_size[1]
+    data = np.empty((0, nb_features), float)
+    for index, row in df.iterrows():
+        path = row.word_img_path
+        new_row = preprocess(path, img_size=img_size,  data_augmentation=True, is_threshold=True).numpy()
+        new_row = new_row.reshape(-1)
+        data = np.append(data, [new_row], axis=0)
+    return data
+
+def get_dataframe_with_preprocessed_imgs(nb_rows = 1000, img_size = (32, 128), load_pickle_if_exists = True, debug=True, pickle_name="letter_detection_data"):
+    full_df = pd.read_pickle('../pickle/df.pickle')
+    if not pickle_name:
+        raise Exception("Cannot have an empty pickle name")
+    pickle_path = "../pickle/" + pickle_name + ".pickle"
+
+    file_exists = os.path.exists(pickle_path)
+    if file_exists and load_pickle_if_exists:
+        if debug: 
+            print("Loading existing data from ", pickle_path, "...")
+        return pickle.load(open(pickle_path, "rb"))
+
+    if debug: 
+        print("Generating data...")
+
+     # Only interested in letters, not punctation or decimal for the moment
+    if debug: 
+        print("Filtering data: taking only letters")
+    r = r'[a-zA-Z]+'
+    df = full_df[full_df['transcription'].str.contains(r)]
+    np.random.seed(seed=42)
+
+    # reducing row
+    if nb_rows >= len(df):
+        nb_rows = len(df)
+        print('DataFrame only contains', len(df), ' rows => using full dataframe')
+    if debug: 
+        print("Using", nb_rows, "rows")
+
+    df = df.iloc[np.random.choice(nb_rows, nb_rows)]
+
+    df['length'] = df['transcription'].apply(lambda x: len(x.strip()))
+    df.rename(columns = {'form_img_path_y': 'form_img_path'}, inplace = True)
+    # reducing columns
+    df = df[['michelson_contrast', 'gray_level_mot', 'word_id', 'gray_level', 'x', 'y', 'w', 'h', 'transcription', 'word_img_path', 'form_img_path', 'length']]
+    df.reset_index(inplace=True)
+
+    if debug: 
+        print("Starting preprocessing of images with tensorflow")
+    
+    preprocessed_imgs = process_df_img(df, img_size)
+    data = {
+        'df': df,
+        'preprocessed_imgs': preprocessed_imgs
+    }
+    if debug: 
+        print("Creating pickle dump", pickle_path)
+    pickle.dump(data, open(pickle_path, "wb" ))
+    return data
+
+def basic_bw_tensor_img_show(tensor_img, ax = None):
+    if ax is None:
+        ax = plt
+    ax.axis('off')
+    ax.imshow(tensor_img.numpy(), cmap='gray');
 
 @tf.function
 def load_image(filepath):
     im = tf.io.read_file(filepath)
     im = tf.image.decode_png(im, channels=0)
     return im
-
-
 
 @tf.function
 def preprocess(filepath, img_size=(32, 128), data_augmentation=False, scale=0.8, is_threshold=False):
@@ -61,3 +178,22 @@ def preprocess(filepath, img_size=(32, 128), data_augmentation=False, scale=0.8,
     if is_threshold:
         return tf.expand_dims(1-(1-img)*tf.cast(img < 0.8, tf.float32), -1)
     return tf.expand_dims(img, -1)
+
+
+def show_mlp_result_for_row(X_test, y_test, y_pred, y_pred_proba, selected_row_index=0):
+    selected_row = X_test[selected_row_index]
+    predict_length = y_pred[selected_row_index]
+
+    plt.figure(figsize=(20,10))
+    plt.subplot(1,2,1)
+    plt.title("Proba d'être un mot de longueur...")
+    plt.axvline(predict_length, color='red', ls='--')
+    plt.axvline(y_test[selected_row_index], color='green')
+    plt.plot(range(1, 1 + len(y_pred_proba[selected_row_index])), y_pred_proba[selected_row_index])
+
+    plt.subplot(1,2,2),
+    plt.title("Longueur de caractères: " + str(predict_length))
+    plt.imshow(selected_row.reshape(32, 128), cmap='gray')
+    plt.axis('off')
+    plt.show()
+
