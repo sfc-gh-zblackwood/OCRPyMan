f65c450e80e30449f74c9c48df1c45bd0ac3f6ae
Jean-Paul BELLA <jean.paul.bella@hotmail.fr>
2022-10-12 22:20:32 +0200

    Text recognition info

---
+++ notebooks/text_recognition/ctc_utils.py
@@ -0,0 +1,11 @@
+import tensorflow as tf
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+import pickle
+import pandas as pd
+import re
+import contextlib
+import os
+import matplotlib.patches as patches
+import random
