e4529b10f091e9087bb1b1c07c06294c8d2d7afa
ddex08 <111470124+ddex08@users.noreply.github.com>
2023-03-14 01:40:19 +0100

    ajout contenu

--- streamlit_app/tabs/intro.py
+++ streamlit_app/tabs/intro.py
@@ -10,14 +10,19 @@ def run():
     st.markdown("---")
     st.markdown(
         """
+        ## Context
         **Optical Character Recognition (OCR)** is nowadays heavily used in many different fields.\n 
         **Recognizing the text in an image** can be used to simply digitalise it or even detect a specific
         structure to later categorize it into a specific kind of document.
         """
         )
     st.markdown("""
-        In this project, we are trying to **implement from scratch** an Handwritten Text Recognition
-        (HTR) model to better understand the underlying deep learning algorithms used in this task.
+        ## Project objectives
+        ##### 1. Implementation of a Handwritten Text Recognition model from scratch
+        Our approach will leverage convolutional neural networks (CNNs) and recurrent neural networks (RNNs) to process and classify images of handwritten text. The model will be trained on a large dataset of labeled handwritten text images to improve its accuracy and generalization capabilities.
+        ##### 2. Implementation of a Text Detection model with the pretrained package docTR
+        ##### 3. Combining the two previous models and present them through a Streamlit web application
+
         To achieve this, we will use the *IAM Dataset*.
         """
     )
