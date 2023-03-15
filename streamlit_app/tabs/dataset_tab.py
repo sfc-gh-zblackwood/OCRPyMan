cdf4a4d072253c86fe24164475ff846025a565ba
ddex08 <111470124+ddex08@users.noreply.github.com>
2023-03-15 01:46:13 +0100

    ajout description dataset

--- streamlit_app/tabs/dataset_tab.py
+++ streamlit_app/tabs/dataset_tab.py
@@ -1,54 +1,101 @@
 import streamlit as st
-import pandas as pd
-import numpy as np
-from PIL import Image
-import cv2
-import matplotlib.pyplot as plt
+#import pandas as pd
+#import numpy as np
+#from PIL import Image
+#import cv2
+#import matplotlib.pyplot as plt
 
-title = "The dataset"
-sidebar_name = "The dataset"
+title = "Dataset"
+sidebar_name = "Dataset"
+
 
 def run():
     # TODO 1 slide de présentation des données (volumétrie, architecture, etc.).
 
     st.title(title)
+    tab1, tab2, tab3 = st.tabs(["Description", "Forms", "Words"])
+    with tab1:
+        show_description()
+    with tab2:
+        show_forms()
+    with tab3:
+        show_words()
+
+
+
+#@st.cache
+#def read_pickle(filepath):
+#    return pd.read_pickle(filepath)
+
+def show_description():
+    st.subheader("Dataset description")
     st.markdown(
         """
-        The first step is to analyse our dataset. 
-
         The IAM Handwriting Database 3.0 is structured as follows:
-        - 657 writers contributed samples of their handwriting
-        - 1'539 pages of scanned text
-        - 5'685 isolated and labeled sentences
-        - 13'353 isolated and labeled text lines
-        - 115'320 isolated and labeled words
-
-        All form, line and word images are provided as **PNG files**.
+        - 657 writers contributed samples of their handwriting,
+        - 1'539 pages of scanned text (forms),
+        - 5'685 isolated and labeled sentences,
+        - 13'353 isolated and labeled text lines,
+        - 115'320 isolated and labeled words (transcription) with the coordinates and size of the
+        respective bounding boxes and a boolean label (seg_res) for segmentation success.
+        
+        The words are all in the English language.
+        All form, line and word images are provided as PNG files in grayscale.
         """
     )
+    st.image("../images/dataset_DB6.png")
+    st.markdown(" ")
 
-    df_before_prepro = read_pickle('../pickle/preprocessing_word_df_before.pickle')
-    df = read_pickle('../pickle/df.pickle')
-    # TODO Add some images of:
-    # - form, 
-    # - lines,
-    # - sentences,
-    # - words
+        
+def show_forms():
+    st.subheader("Forms")
+    st.markdown(
+        """
+        The following Figure shows 10 random forms from the 1539 forms on the dataset.
+        """)
+    st.image("../images/dataset_DB2.png")
+    
+    st.markdown(
+        """
+        The images in the dataset are all produced in good conditions in terms of the contrast between the text and the background. Generally, there are no bumps, colors or patterns that can make text extraction difficult. Here are the min and max gray level forms:
+        """)
+    st.image("../images/dataset_DB3.png")
 
     st.markdown(
         """
-        The dataset is exclusively composed of english words. As a consequence, our OCR will perform significantly better 
-        when detecting the english language. 
+        ***
+        The following Figure shows a heatmap of distribution of words on the form format. We can see a uniform distribution along a linear pattern of evenly spaced lines.
+        """)
+    st.image("../images/text_box_position.png")
+    
+    st.markdown(" ")
+    
+            
+def show_words():
+    st.subheader("Words")
+    st.markdown(
         """
-    )
+        The following Figure shows 50 of the 115320 images of words on the dataset:
+        """)
+    st.image("../images/dataset_DB4.png")
+    
+    st.markdown(
+        """
+        This last Figure shows a great variability of handwriting images on the dataset: styles, sizes, thickness, contrast and background noise. We can also identify the presence of punctuation on the dataset and this subject will be addressed in the cleaning steps of the pre-processing phase. Nonetheless, images of regular words are generally recognizable by a human and correspond to the respective transcriptions.
+        """)
+    st.markdown(
+        """
+        ***
+        The Figure below is a word cloud of the dataset and gives the reader an intuition of the most common words and characters.
+        """)
+    st.image("../images/dataset_DB5.png")
 
+    st.markdown(
+        """
+        ***
+        The following scatter plot shows characters of the english language plotted with x as percentage of the english language and y as percentage in our dataset. The distribution is approximately linear which indicates the corpus of our dataset is representative of the english language.
+        """)
+    st.image("../images/dataset_DB8.png")
 
-
-
-    st.write("However, only")
-    st.write(len(df), "words out of ", len(df_before_prepro), " initials have been used. Why ? Because a *processing* phase was required.")
-
-
-@st.cache
-def read_pickle(filepath):
-    return pd.read_pickle(filepath)
+    st.markdown(" ")
+    
--- streamlit_app/tabs/intro.py
+++ streamlit_app/tabs/intro.py
@@ -8,21 +8,27 @@ def run():
     st.image("https://dst-studio-template.s3.eu-west-3.amazonaws.com/2.gif")
     st.title("OCRpyMAN")
     st.markdown("---")
+
+    st.subheader("Context")
+    
     st.markdown(
         """
-        ## Context
         **Optical Character Recognition (OCR)** is nowadays heavily used in many different fields.\n 
         **Recognizing the text in an image** can be used to simply digitalise it or even detect a specific
         structure to later categorize it into a specific kind of document.
         """
         )
+    
+    st.subheader("Project objectives")
+    
     st.markdown("""
-        ## Project objectives
         ##### 1. Implementation of a Handwritten Text Recognition model from scratch
         Our approach will leverage convolutional neural networks (CNNs) and recurrent neural networks (RNNs) to process and classify images of handwritten text. The model will be trained on a large dataset of labeled handwritten text images to improve its accuracy and generalization capabilities.
         ##### 2. Implementation of a Text Detection model with the pretrained package docTR
         ##### 3. Combining the two previous models and present them through a Streamlit web application
 
         To achieve this, we will use the *IAM Dataset*.
+
+       
         """
     )
