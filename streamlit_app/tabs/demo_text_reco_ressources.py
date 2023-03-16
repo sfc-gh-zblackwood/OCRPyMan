2ee6e4918bcd270f94008add983c8f45660a76b7
Montikore <t.joassard@gmail.com>
2023-03-16 15:29:08 +0100

    ajout demo text reco + images

---
+++ streamlit_app/tabs/demo_text_reco_ressources.py
@@ -0,0 +1,108 @@
+import streamlit as st
+import tensorflow as tf
+import matplotlib.pyplot as plt
+
+
+import sys
+sys.path.insert(1, '../notebooks')
+import preprocessing as pp
+import letter_detection_utils as ld_util
+import ressources as rss
+import modele as mdl
+
+
+
+models = [['tj_ctc_base_10epochs_LRe-4', 'Original dataset, 10 epochs, LR 1e-4'],
+          ['tj_ctc_base_20epochs_LRe-4', 'Original dataset, 20 epochs, LR 1e-4'],
+          ['tj_ctc_base_20epochs_LR-plateau', 'Original dataset, 20 epochs, LR on Plateau'],
+          ['tj_ctc_augmented_20epochs_LR-plateau', 'Augmented dataset, 20 epochs, LR on Plateau']]
+
+data_extract_words = '../images/data_extract/words'
+
+
+def show_data_extract():
+    rss.init()
+    
+    st.write("See below the predictions with 4 different models for a selection of images.")
+
+    #liste les fichiers
+    images = load_images_from_path(data_extract_words)
+    
+    for model, title in models:           
+        desc = f"""Predictions using a model with : **{title}**"""
+        st.write(desc)
+        texts = get_predictions(model, images)
+        show_predictions_images(images, texts)
+        
+        st.text("")  # saut de ligne
+        st.text("")  # saut de ligne
+    
+       
+def show_local():
+    st.write("Browse your local files to get an image prediction")
+        
+        
+def show_drawing():
+    st.write("Use your mouse to write and get a prediction")    
+
+
+@st.cache  
+def get_predictions(model, images):
+    loaded_model = tf.keras.models.load_model("../pickle/"+model, custom_objects={"CTCLoss": mdl.CTCLoss})
+    
+    text_probs = loaded_model.predict(images) 
+    text = ld_util.greedy_decoder(text_probs, rss.charList)
+    
+    return text
+
+
+
+def load_images_from_path(path):
+    
+    #liste les fichiers
+    images_path = pp.get_files(path, ext='png', sub=False)
+    
+    word_imgs = tf.zeros([1, 32, 128, 1])
+    
+    for image_path in images_path:
+        image  = ld_util.load_image(image_path)       
+        image = ld_util.preprocess(image, img_size=rss.img_size,  data_augmentation=True, is_threshold=True)
+        image = tf.expand_dims([image], -1)
+        image = tf.squeeze(image, [3])
+        word_imgs = tf.concat([word_imgs, image], 0)
+        
+        
+    # for i in range(len(word_imgs_prepro)):
+    #         img = ld_util.process_1_img_from_form(img_path, *bounding_boxes_xyhw[i])
+    #         img = tf.expand_dims([img], -1)
+    #         img = tf.squeeze(img, [3])
+    #     else:
+    #         img = word_imgs[i] / 255
+    #     word_imgs_prepro = tf.concat([word_imgs_prepro, img], 0)
+    
+    
+    # Removing extra img due to initialization
+    word_imgs = word_imgs[1:]
+    
+    return word_imgs
+
+
+def show_predictions_images(images, textes):
+    fig = plt.figure(figsize=(20, 12))
+
+    lin = 5
+    col = 6
+    
+    # Parcourir les images et les étiquettes
+    for i in range(len(textes)):
+        img = images[i]
+        label = textes[i]
+
+        # Ajouter la sous-figure à la figure principale
+        fig.add_subplot(lin, col, i+1)
+        plt.imshow(img, cmap='gray')
+        plt.title(label, fontsize=20)
+        plt.axis('off')
+
+    # Afficher la figure dans Streamlit
+    st.pyplot(fig)
---
+++ streamlit_app/tabs/demo_text_recognition.py
@@ -0,0 +1,31 @@
+import streamlit as st
+from tabs import demo_text_reco_ressources as demo
+
+title = "Demo Text Recognition"
+sidebar_name = "Demo Text Recognition"
+
+
+def run():
+    st.title(title)
+    st.write("This page is intended to let you use the models we created.")
+    st.markdown(
+        """
+        There is 3 ways to use them : 
+        - Drawing with the mouse  
+        - Using an extract from the original data : 15 images ramdomly choosed
+        - Uploading a local image 
+              
+        """)
+    st.markdown("**There is no spellchecking on these predictions**")
+    
+    tab1, tab2, tab3 = st.tabs(["Drawing", "Data extract", "Local image"])
+    # st.write(tab1)
+    # st.write(tab2)
+    with tab1:
+        demo.show_drawing()
+
+    with tab2:
+        demo.show_data_extract()
+        
+    with tab3:
+        demo.show_local()
